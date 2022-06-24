9f46d3713b888da72ebc84f2d0178c5f90af43e1
Wallace Júnior <wflj1997@hotmail.com>
2022-06-24 01:19:00 -0300

    adicionar link para homepage na logo do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
 import { Logo } from "./Logo";
 
 export const Header: React.FC = () => (
   <header className="flex w-full items-center justify-center border-b border-gray-600 bg-gray-700 py-5">
-    <Logo />
+    <Link to="/" className="cursor-pointer">
+      <Logo />
+    </Link>
   </header>
 );
