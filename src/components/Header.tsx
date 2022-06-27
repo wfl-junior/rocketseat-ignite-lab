2a882a714acdf4d44e8245709775bf28995e5541
Wallace Júnior <wflj1997@hotmail.com>
2022-06-27 13:59:37 -0300

    sticky header na página de Event

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { Logo } from "./Logo";
 
 export const Header: React.FC = () => (
-  <header className="flex w-full items-center justify-center border-b border-gray-600 bg-gray-700 py-5">
+  <header className="sticky top-0 z-[100] flex w-full items-center justify-center border-b border-gray-600 bg-gray-700 py-5">
     <Link to="/" className="cursor-pointer">
       <Logo />
     </Link>
